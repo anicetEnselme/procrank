ca2f1272c3380ec02947ada15cb46c1c72dcb9d6
Enselme Anicet <enselme20@gmail.com>
2020-09-23 01:53:41 +0100

    reviewed the displaying

--- module/procrank.c
+++ module/procrank.c
@@ -15,7 +15,7 @@ static int procinfo_proc_show(struct seq_file *m, void *v)
 {
 	struct task_struct *p; 
 	int nr_process = 0;
-	int test = 0;
+	//int test = 0;
 
 	//test = emery();
 	for_each_process(p) // Traverse all processes
@@ -25,7 +25,7 @@ static int procinfo_proc_show(struct seq_file *m, void *v)
 		u64 pss;
 		
 		printk(KERN_INFO "Iteration \"%d\" \n", nr_process);
-		seq_printf(m, "Process_NAME: %s 		 ===> PID: %u\n", p->comm, p->pid);	// print PID
+		seq_printf(m, "NAME: %s, PID: %u", p->comm, p->pid);	// print PID
         printk(KERN_INFO "I am here");
         if (nr_process == 1)
         {
@@ -37,12 +37,16 @@ static int procinfo_proc_show(struct seq_file *m, void *v)
 				printk(KERN_INFO "I am here 2 ");
 				pss = gather_procrank(vma);
 				printk(KERN_INFO "I am here 3");
-				seq_printf(m, "PSS : %lld ", pss);
 				printk(KERN_INFO "Je suis apres le seq_printf");
 			}
 			printk(KERN_INFO "I am here4");
+			seq_printf(m, " PSS : %lld \n", pss);
 
 			nr_process += 1;					
+		}
+		else{
+			seq_printf(m, " PSS zero : %d \n", 0);
+
 		}    	
     }
 	seq_printf(m, "number of process: %d\n", nr_process);	// print number of process
